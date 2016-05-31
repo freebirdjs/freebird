142b15c1bff82d9735942840887d532a4cdc801c
Simen Li <simenkid@gmail.com>
2016-05-31 18:31:29 +0800

    testing: events checking

--- test/freebird.test.js
+++ test/freebird.test.js
@@ -26,39 +26,114 @@ fbird.registerNetcore(ncMock, function (err, nc) {
         console.log('nc');
 });
 
-
 fbird.on('error', function (err) {
-    console.log(err);
+    console.log('>> FB:error');
 });
 
-fbird.on('devIncoming', function (data) {
-    console.log('>>>>>>>> devIncoming');
-    // console.log(err);
-    // console.log(data.dev._gads);
+fbird.on('devIncoming', function (msg) {
+    console.log('>> FB:devIncoming');
+    // console.log(msg);
 });
 
-fbird.on('gadIncoming', function (data) {
-    console.log('>>>>>>>> gadIncoming');
-    // console.log(err);
-    // console.log(data.gad.getDev()._gads);
+fbird.on('devLeaving', function (msg) {
+    console.log('>> FB:devLeaving');
+    console.log(msg);
 });
 
+fbird.on('devReporting', function (msg) {
+    console.log('>> FB:devReporting');
+});
 
-// fbird.on('devIncoming', function (data) {
-//     console.log('>>>>>>>> devIncoming');
-//     // console.log(err);
-//     console.log(data.dev);
-// });
+fbird.on('bannedDevIncoming', function (msg) {
+    console.log('>> FB:bannedDevIncoming');
+});
 
+fbird.on('bannedDevReporting', function (msg) {
+    console.log('>> FB:bannedDevReporting');
+});
 
-fbird.on('_nc:devIncoming', function (data) {
-    console.log('_nc:devIncoming');
-    // console.log(err);
-    //console.log(data);
+fbird.on('gadIncoming', function (msg) {
+    console.log('>> FB:gadIncoming');
+});
+
+fbird.on('gadLeaving', function (msg) {
+    console.log('>> FB:gadLeaving');
+});
+
+fbird.on('bannedGadIncoming', function (msg) {
+    console.log('>> FB:bannedGadIncoming');
+});
+
+fbird.on('bannedGadReporting', function (msg) {
+    console.log('>> FB:bannedGadReporting');
+});
+
+fbird.on('gadReporting', function (msg) {
+    console.log('>> FB:gadReporting');
+    console.log({ gad: msg.gad.getId(), data: msg.data });
+});
+
+fbird.on('netReady', function (msg) {
+    console.log('>> FB:netReady');
+});
+
+fbird.on('permitJoin', function (msg) {
+    // console.log('>> FB:permitJoin');
+    // console.log({ netcore: msg.netcore.getName(), timeLeft: msg.timeLeft });
+});
+
+fbird.on('started', function (msg) {
+    console.log('>> FB:started');
+});
+
+fbird.on('stopped', function (msg) {
+    console.log('>> FB:stopped');
+});
+
+fbird.on('enabled', function (msg) {
+    console.log('>> FB:enabled');
+});
+
+fbird.on('disabled', function (msg) {
+    console.log('>> FB:disabled');
+});
+
+fbird.on('netChanged', function (msg) {
+    // console.log('>> FB:netChanged');
+    // console.log({ dev: msg.dev.getId(), data: msg.data });
+});
+
+fbird.on('statusChanged', function (msg) {
+    // console.log('>> FB:statusChanged');
+    // console.log({ dev: msg.dev.getId(), data: msg.data });
+});
+
+fbird.on('devPropsChanged', function (msg) {
+    console.log('>> FB:devPropsChanged');
+});
+
+fbird.on('devAttrsChanged', function (msg) {
+    // console.log('>> FB:devAttrsChanged');
+    // console.log({ dev: msg.dev.getId(), data: msg.data });
+});
+
+fbird.on('panelChanged', function (msg) {
+    // console.log('>> FB:panelChanged');
+    // console.log({ gad: msg.gad.getId(), data: msg.data });
+});
+
+fbird.on('gadPropsChanged', function (msg) {
+    console.log('>> FB:gadPropsChanged');
+    console.log(msg);
+});
+
+fbird.on('gadAttrsChanged', function (msg) {
+    // console.log('>> FB:gadAttrsChanged');
+    // console.log({ gad: msg.gad.getId(), data: msg.data });
+
 });
 
 fbird.start(function (err) {
-    console.log('start');
     fbird.net.permitJoin('mock', 180);
-    // console.log(ncMock._fb);
 });
+
