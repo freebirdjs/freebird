b53487a36bb9ab7eb36ffe07baa1b3c3e756f525
Jack Wu <jackchased@gmail.com>
2017-02-16 21:36:39 +0800

    integration testing

--- test/freebird.intergration.jack.test.js
+++ test/freebird.intergration.jack.test.js
@@ -276,60 +276,281 @@ describe('Test', function () {
         });
     });
 
-    describe('#NcDevIncoming', function () {
+    describe('#event - devIncoming', function () {
         it('new device incoming, so not exist database', function (done) {
-            this.timeout(10000);
+            this.timeout(15000);
+            var incomingCalled, devEnableCalled, devOnlineCalled,
+                checkDone = function () {
+                    if (incomingCalled && devEnableCalled && devOnlineCalled) {
+                        fbird.removeListener(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+                        done();
+                    }
+                },
+                netChangeLsn = function (msg) {
+                    if (msg.permAddr === 'AA:BB:CC:DD:EE:01' && msg.data.status === 'online') {
+                        devOnlineCalled = true;
+                        checkDone();
+                    } else if (msg.permAddr === 'AA:BB:CC:DD:EE:01' && msg.data.enabled === true) {
+                        devEnableCalled = true;
+                        checkDone();
+                    }
+                };
 
+            fbird.on(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
             fbird.once(EVT_TOP.DEV_INCOMING, function (dev) {
                 fbird.permitJoin(0);
-                if (dev.ncName === 'mock01' && dev.permAddr === 'AA:BB:CC:DD:EE:01')
+                if (dev.ncName === 'mock01' && dev.permAddr === 'AA:BB:CC:DD:EE:01') {
+                    incomingCalled = true;
+                    checkDone();
+                }
+            });
+
+            fbird.permitJoin(60);
+        });
+
+        it('old device incoming, device exist database', function (done) {
+            this.timeout(10000);
+            var devMock1 = fbird.findByNet('device', 'mock01', 'AA:BB:CC:DD:EE:01'),
+                oldInfo = devMock1.get('net'),
+                newInfo,
+                netChangeLsn = function (msg) {
+                    if (msg.data.status === 'online' && msg.permAddr === 'AA:BB:CC:DD:EE:01') {
+                        fbird.permitJoin(0);
+
+                        newInfo = devMock1.get('net');
+                        delete oldInfo.joinTime;
+                        delete oldInfo.timestamp;
+                        delete newInfo.joinTime;
+                        delete newInfo.timestamp;
+
+                        if (_.isEqual(oldInfo, newInfo)) {
+                            fbird.removeListener(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+                            done();
+                        }
+                    }
+                };
+
+            devMock1.set('net', {
+                role: 'router',
+                parent: '7',
+                maySleep: false,
+                sleepPeriod: 120,
+                address: { permanent: 'AA:BB:CC:DD:EE:01', dynamic: '200.200.200.1' }
+            });
+
+            fbird.on(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+
+            ncMock1._controller._DeviceLeave('AA:BB:CC:DD:EE:01');
+            fbird.permitJoin(60);
+        });
+    });
+
+    describe('#event - gadIncoming', function () {
+        it('new gadget incoming, so not exist database', function (done) {
+            this.timeout(10000);
+            var incomingCalled, devEnableCalled, devOnlineCalled,
+                pwrCtrlIncoming, temperaturelIncoming,
+                checkDone = function () {
+                    if (incomingCalled && devEnableCalled && devOnlineCalled) {
+                        fbird.removeListener(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+                        fbird.removeListener(EVT_TOP.GAD_INCOMING, gadIncomingLsn);
+                        done();
+                    }
+                },
+                netChangeLsn = function (msg) {
+                    if (msg.permAddr === 'AA:BB:CC:DD:EE:02' && msg.data.status === 'online') {
+                        devOnlineCalled = true;
+                        checkDone();
+                    } else if (msg.permAddr === 'AA:BB:CC:DD:EE:02' && msg.data.enabled === true) {
+                        devEnableCalled = true;
+                        checkDone();
+                    }
+                },
+                gadIncomingLsn = function (gad) {
+                    if (gad.ncName === 'mock01' && gad.permAddr === 'AA:BB:CC:DD:EE:02') {
+                        if (gad.auxId === 'pwrCtrl/0')
+                            pwrCtrlIncoming = true;
+                        else if (gad.auxId === 'temperature/0')
+                            temperaturelIncoming = true;
+
+                        if (pwrCtrlIncoming && temperaturelIncoming) {
+                            fbird.permitJoin(0);
+                            incomingCalled = true;
+                            checkDone();
+                        }
+                    }
+                };
+
+            fbird.on(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+            fbird.on(EVT_TOP.GAD_INCOMING, gadIncomingLsn);
+            fbird.once(EVT_TOP.DEV_INCOMING, function (dev) {
+                
+                if (dev.ncName === 'mock01' && dev.permAddr === 'AA:BB:CC:DD:EE:02') {
+                    incomingCalled = true;
+                    checkDone();
+                }
+            });
+            fbird.permitJoin(60);
+        });
+
+        it('old gadget incoming, gadget exist database', function (done) {
+            this.timeout(10000);
+            var oldGadMock1 = fbird.findByNet('gadget', 'mock01', 'AA:BB:CC:DD:EE:02', 'pwrCtrl/0'),
+                oldAttrs = oldGadMock1.get('attrs'),
+                incomingCalled, devOnlineCalled, gadAttrsChangeCalled,
+                pwrCtrlIncoming, temperaturelIncoming,
+                checkDone = function () {
+                    if (incomingCalled && devOnlineCalled && gadAttrsChangeCalled) {
+                        fbird.removeListener(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+                        fbird.removeListener(EVT_BTM.NcGadIncoming, gadIncomingLsn);
+                        fbird.removeListener(EVT_TOP.GAD_ATTRS_CHANGED, gadAttrsChangeLsn);
+                        done();
+                    }
+                },
+                netChangeLsn = function (msg) {
+                    if (msg.permAddr === 'AA:BB:CC:DD:EE:02' && msg.data.status === 'online') {
+                        devOnlineCalled = true;
+                        checkDone();
+                    }
+                },
+                gadIncomingLsn = function (gad) {
+                    if (gad.ncName === 'mock01' && gad.permAddr === 'AA:BB:CC:DD:EE:02') {
+                        if (gad.auxId === 'pwrCtrl/0')
+                            pwrCtrlIncoming = true;
+                        else if (gad.auxId === 'temperature/0')
+                            temperaturelIncoming = true;
+
+                        if (pwrCtrlIncoming && temperaturelIncoming) {
+                            fbird.permitJoin(0);
+                            incomingCalled = true;
+                            checkDone();
+                        }
+                    }
+                },
+                gadAttrsChangeLsn = function (msg) {
+                    if (msg.ncName === 'mock01' && msg.permAddr === 'AA:BB:CC:DD:EE:02' && _.isEqual(oldAttrs, msg.data)) {
+                        gadAttrsChangeCalled = true;
+                        checkDone();
+                    }
+                };
+
+            oldGadMock1.set('attrs', {
+                onOff: true,
+                dimmer: 128,
+                onTime: 77,
+            });
+
+            fbird.on(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+            fbird.on(EVT_BTM.NcGadIncoming, gadIncomingLsn);
+            fbird.on(EVT_TOP.GAD_ATTRS_CHANGED, gadAttrsChangeLsn);
+            ncMock1._controller._DeviceLeave('AA:BB:CC:DD:EE:02');
+            fbird.permitJoin(60);
+        });
+    });
+
+    describe('#event - devNetChanging', function () {
+        it('device net information change', function (done) {
+            this.timeout(10000);
+            var netChangeLsn = function (msg) {
+                    var dev = fbird.findByNet('device', msg.ncName, msg.permAddr),
+                        netInfo = dev.get('net');
+
+                    if (_.isEqual(netInfo.address.dynamic, msg.data.address.dynamic))
+                        done();
+                };
+
+            fbird.once(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+        });
+    });
+
+    describe('#event - devReporting', function () {
+        it('device attribute reporting', function (done) {
+            this.timeout(10000);
+            var devReportingLsn = function (msg) {
+                    var dev = fbird.findByNet('device', msg.ncName, msg.permAddr),
+                        netAttrs = dev.get('attrs');
+
+                    if (msg.data.version && _.isEqual(netAttrs.version.hw, msg.data.version.hw)) {
+                        fbird.removeListener(EVT_TOP.DEV_REPORTING, devReportingLsn);
+                        done();
+                    }
+                };
+
+            fbird.on(EVT_TOP.DEV_REPORTING, devReportingLsn);
+        });
+    });
+
+    describe('#event - gadReporting', function () {
+        it('gadget attribute reporting', function (done) {
+            this.timeout(10000);
+            var gadReportingLsn = function (msg) {
+                    var gad = fbird.findByNet('gadget', msg.ncName, msg.permAddr, msg.auxId),
+                        gadAttrs = gad.get('attrs'),
+                        attrKey = _.keys(msg.data)[0];
+
+                    if (_.isEqual(gadAttrs[attrKey], msg.data[attrKey]))
+                        done();
+                };
+
+            fbird.once(EVT_TOP.GAD_REPORTING, gadReportingLsn);
+        });
+
+        it('gadget attribute reporting - appendFlag', function (done) {
+            this.timeout(30000);
+
+            fbird.once(EVT_BTM.GadAttrsAppend, function (msg) {
+                var gad = fbird.findByNet('gadget', msg.ncName, msg.permAddr, msg.auxId),
+                    gadAttrs = gad.get('attrs'),
+                    attrKey = _.keys(msg.data)[0];
+
+                if (_.isEqual(gadAttrs[attrKey], msg.data[attrKey]))
                     done();
             });
 
-            fbird.permitJoin(60);
+            ncMock1._controller._gadAttrRandomChanges('gad', true);
         });
+    });
 
-        it('old device incoming, so exist database', function (done) {
+    describe('#event - devLeaving', function () {
+        it('device offline', function (done) {
             this.timeout(10000);
+            var netChangeLsn = function (msg) {
+                    if (msg.data.status === 'offline' && msg.permAddr === 'AA:BB:CC:DD:EE:02'){
+                        fbird.permitJoin(0);
+                        fbird.removeListener(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
+                        done();
+                    }
+                };
 
-            var devMock1 = fbird.findByNet('device', 'mock01', 'AA:BB:CC:DD:EE:01');
-            fbird.on(EVT_TOP.DEV_NET_CHANGED, function () {
-                console.log(arguments);
-            });
+            fbird.on(EVT_TOP.DEV_NET_CHANGED, netChangeLsn);
 
-            ncMock1._controller._DeviceLeave('AA:BB:CC:DD:EE:01');
-
-            fbird.once(EVT_TOP.DEV_INCOMING, function (dev) {
-                fbird.permitJoin(0);
-                done();
-            });
-
+            ncMock1._controller._DeviceLeave('AA:BB:CC:DD:EE:02');
             fbird.permitJoin(60);
         });
-    });
 
-    describe('#NcGadIncoming', function () {
-        it(' ', function () {
-        });
-    });
+        it('manually remove device', function (done) {
+            this.timeout(10000);
+            var removeCbCalled, devLeavingCalled,
+                checkDone = function () {
+                    if (removeCbCalled && devLeavingCalled)
+                        done();
+                },
+                devLeavingLsn = function (msg) {
+                    if (msg.ncName === 'mock01' && msg.permAddr === 'AA:BB:CC:DD:EE:01'){
+                        fbird.removeListener(EVT_TOP.DEV_LEAVING, devLeavingLsn);
+                        devLeavingCalled = true;
+                        checkDone();
+                    }
+                };
 
-    describe('#NcDevNetChanging', function () {
-        it(' ', function () {
-        });
-    });
+            fbird.on(EVT_TOP.DEV_LEAVING, devLeavingLsn);
 
-    describe('#NcDevReporting', function () {
-        it(' ', function () {
-        });
-    });
-
-    describe('#NcGadReporting', function () {
-        it(' ', function () {
-        });
-    });
-
-    describe('#NcDevLeaving', function () {
-        it(' ', function () {
+            fbird.remove('mock01', 'AA:BB:CC:DD:EE:01', function (err, permAddr) {
+                if (!err && permAddr === 'AA:BB:CC:DD:EE:01') {
+                    removeCbCalled = true;
+                    checkDone();
+                }
+            });
         });
     });
 
